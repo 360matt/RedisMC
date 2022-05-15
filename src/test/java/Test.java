3126223279c5bbbe174e14afec1dd5983bab023d
360matt <mattbacq2004@gmail.com>
2022-05-15 15:48:29 +0200

    Change example file

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,21 +1,19 @@
 import fr.i360matt.redismc.RedisClient;
-import fr.i360matt.redismc.RedisConnection;
 import fr.i360matt.redismc.Storage;
-import fr.i360matt.redismc.storage.RedisHashMap;
-import fr.i360matt.redismc.storage.RedisList;
-
-import java.util.Arrays;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        RedisClient.create(
-                new RedisConnection("localhost", 6379),
-                "rouge",
-                "couleur"
-        );
+        RedisClient.create((auth) -> {
+            auth.setHost("localhost");
+            auth.setPort(6379);
+            auth.setPassword("");
+            auth.setName("rouge");
+            auth.setGroup("couleur");
+        });
 
+        /*
         RedisHashMap<Integer, String> map = Storage.getMap("liste");
         map.put(1, "rouge");
         map.put(2, "bleu");
@@ -23,7 +21,19 @@ public class Test {
         System.out.println(map.get(1));
         System.out.println(map.get(2));
 
-        map.clear();
+        map.clear();*/
+
+
+        Storage.getMap("liste", (map2) -> {
+            map2.put(1, "rouge");
+            map2.put(2, "bleu");
+
+            System.out.println(map2.get(1));
+            System.out.println(map2.get(2));
+
+            map2.clear();
+        });
+
 
     }
 
